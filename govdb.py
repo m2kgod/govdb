8c327837a5e29578b311caffa48bd03da065a122
ph03n1x <55708713+m2kgod@users.noreply.github.com>
2020-08-04 03:50:33 -0400

    Add files via upload

---
+++ govdb.py
@@ -0,0 +1,80 @@
+print("     "+"*" *62)
+print("     "+"+"+" " *60+"+")
+print("     "+"+"+" " *60+"+")
+print("     "+"+"+" " *8+"  ____    ___   __     __      ____    ____  "+" "*7+"+") 
+print("     "+"+"+" " *8+" / ___|  / _ \  \ \   / /     |  _ \  | __ ) "+" "*7+"+")
+print("     "+"+"+" " *8+"| |  _  | | | |  \ \ / /      | | | | |  _ \ "+" "*7+"+")
+print("     "+"+"+" " *8+"| |_| | | |_| |   \ V /    _  | |_| | | |_) |"+" "*7+"+")
+print("     "+"+"+" " *8+" \____|  \___/     \_/    (_) |____/  |____/ "+" "*7+"+")
+print("     "+"+"+" " *60+"+")
+print("     "+"+" +" " *60+"+")
+print("     "+ "*"*62)
+
+from tqdm import tqdm 
+  
+for i in tqdm (range (100), desc="\n Opening GOVERNMENT DATABASE..."): 
+    pass
+                                         
+cn = input("\n Enter the user's country of origin : ")
+print("\n")
+from tqdm import tqdm 
+  
+for i in tqdm (range (100), desc="\n Fetching country information..."): 
+    pass
+
+
+name = input("\n Enter user name:")
+dad = input("\n Enter user's father name : ")
+mom = input("\n Enter user's mother name : ")
+sex = input("\n Enter the sex of user (male/female): ")
+
+
+import random
+import string
+
+def get_random_string(length):
+    letters = string.ascii_lowercase
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    print ("user's one time username is : "+ result_str)
+     
+
+get_random_string(8)
+
+import random
+import string
+
+def get_random_string(length):
+    letters = string.ascii_lowercase
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    print ("user's one time password is : "+ result_str)
+
+get_random_string(6)
+
+usr=input("\n Enter the login username for " + name + " : ")
+pas=input("\n Enter the login password for " + name + " : ")
+
+from tqdm import tqdm 
+  
+for i in tqdm (range (100), desc="\n Searching user information..."): 
+    pass
+from tqdm import tqdm 
+  
+for i in tqdm (range (100), desc="\n Searching user information..."): 
+    pass
+from tqdm import tqdm 
+  
+for i in tqdm (range (100), desc="\n Searching user information..."): 
+    pass
+from tqdm import tqdm 
+  
+for i in tqdm (range (100), desc="\n Searching user information..."): 
+    pass
+from tqdm import tqdm 
+  
+for i in tqdm (range (100), desc="\n Searching user information..."): 
+    pass
+
+
+
+print("\n\t\t\t\t\t USER " + name + " NOT FOUND IN " + cn + ".govt")
+
